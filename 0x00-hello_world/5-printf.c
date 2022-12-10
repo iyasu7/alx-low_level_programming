d052590451e7d8026f1cc16db29cea8ac429bb82
iyasu7 <iyasuelsabeth7@gmail.com>
2022-12-10 05:58:17 -0800

    FirstC

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints some message
+*
+* Uses printf to print the message
+* Return: Return 0 (Without warning)
+*/
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
