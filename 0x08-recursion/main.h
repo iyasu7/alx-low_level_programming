082fca746a49455802c79576da09c690dc6ac17e
iyasu7 <iyasuelsabeth7@gmail.com>
2023-01-04 05:27:38 -0800

    recursive

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This header file contains all the prototypes required for the
+ * 0x08-recursion project.
+ *
+ * @author: Gizachew Bayness
+ *
+ * Date crated: April 27, 2022
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
